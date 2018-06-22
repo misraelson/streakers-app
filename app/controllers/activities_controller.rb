0994d1cd7bcec61d7526be911822ee14b2138659
misraelson <misraelson@gmail.com>
2018-06-22 16:26:40 -0700

    Implemente Devise gem for user model

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /activities
   def index
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ApplicationRecord
   enum status: { active: 0, finished: 1 }
+
+  has_many :streaks
 end
