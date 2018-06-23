b3cac55858382574fe4eecf84646df0a00de853c
misraelson <misraelson@gmail.com>
2018-06-22 17:14:03 -0700

    migrate streaks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_231024) do
+ActiveRecord::Schema.define(version: 2018_06_23_000950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_06_22_231024) do
   create_table "activities", force: :cascade do |t|
     t.string "title"
     t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "streaks", force: :cascade do |t|
+    t.integer "current_streak"
+    t.date "start_date"
+    t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
