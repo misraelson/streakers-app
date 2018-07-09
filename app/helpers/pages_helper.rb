8b09076b92c730e1c268935695e09bfe06ba0deb
misraelson <misraelson@gmail.com>
2018-07-09 10:23:48 -0700

    refactor titles helper method to cover edge case where there are not duplicate titles and uniq! method returns nil

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,21 +1,24 @@
 module PagesHelper
 
   def activity_title_helper
-    streaks = current_user.streaks.order! 'created_at DESC'
     activity_ids = []
+    titles = []
+    streaks = current_user.streaks.order! 'created_at DESC'
     streaks.each do |aid|
       activity_ids << aid.activity_id
     end
-    titles = []
     activity_ids.each do |f|
       a = Activity.find(f)
       titles << a.title
     end
-    return raw titles.uniq!
+    if titles.uniq! == nil
+      return raw titles
+    else
+      return raw titles.uniq!
+    end
   end
 
   def activity_streaks_data_helper
-    # maybe query current_user.activities active?
     # create a variable that is all streaks ordered by created_at DESC then loop through all those streaks and filter by unique activity_id
     # then shovel current_streak into empty numbers array
     numbers = []
