747e33abf79128e1417da67097a5f49256d4e79a
misraelson <misraelson@gmail.com>
2018-07-31 13:22:19 -0700

    bug fix for showing activities with no streaks in activities index

--- app/jobs/check_streak_reset_job.rb
+++ app/jobs/check_streak_reset_job.rb
@@ -2,12 +2,12 @@ class CheckStreakResetJob < ApplicationJob
 
   def perform
     # find all active streaks 👇
-    streaks = Streak.all.where(status: "active")
+    streaks = Streak.where(status: "active")
     # loop through each streak => if reset value is == true (which happens on update action) then flip it to false
     # else status = "finished"
     streaks.each do |streak|
       if streak.reset?
-       streak.update!(reset: "false")
+       streak.update!(reset: false)
       else
        streak.update!(status: 1, end_date: Time.now)
       end
