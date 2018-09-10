9112cdce6a9398ac2146b21a004605ff5e852b5f
misraelson <misraelson@gmail.com>
2018-09-10 14:48:52 -0700

    config concurrency of sidekiq server

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec rails s
 api: bin/rake db:migrate
-worker: bundle exec sidekiq -e production -t 25
+worker: bundle exec sidekiq -e production -c 3 -t 25
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,9 @@ Sidekiq.configure_server do |config|
   # DEVELOPMENT
   # config.redis = { url: 'redis://localhost:6379/0'  }
   # PRODUCTION
-  config.redis = { url: ENV['REDIS_URL']  }
+  config.redis = {  url: ENV['REDIS_URL'],
+                    size: 7 
+                  }
   schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
@@ -13,5 +15,7 @@ Sidekiq.configure_client do |config|
   # DEVELOPMENT
   # config.redis = { url: 'redis://localhost:6379/0'  }
   # PRODUCTION
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = {  url: ENV['REDIS_URL'],
+                    size: 1
+                  }
 end
