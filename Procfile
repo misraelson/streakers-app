de281a2fef2dfdcfffb1e9e14e94a066fd44e44c
misraelson <misraelson@gmail.com>
2018-09-10 13:53:02 -0700

    config Procfile to boot sidekiq on Heroku and config sidekiq.rb initializer to point to ENV[REDIS_URL]

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec rails s
 api: bin/rake db:migrate
+worker: bundle exec sidekiq -t 25
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
+  # DEVELOPMENT
+  # config.redis = { url: 'redis://localhost:6379/0'  }
+  # PRODUCTION
+  config.redis = { url: ENV['REDIS_URL']  }
   schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
@@ -7,5 +10,8 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
+  # DEVELOPMENT
+  # config.redis = { url: 'redis://localhost:6379/0'  }
+  # PRODUCTION
+  config.redis = { url: ENV['REDIS_URL'] }
 end
