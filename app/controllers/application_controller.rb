436f6ad49af75b9f9b453934f9d1894be8a3d4fc
misraelson <misraelson@gmail.com>
2018-09-11 18:34:09 -0700

    revert application_controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery with: :null_session
   include DeviseWhitelist
   acts_as_token_authentication_handler_for User, fallback: :none
 end
