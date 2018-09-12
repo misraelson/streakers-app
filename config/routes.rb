b7e65b4145dae02548ee01910726496d2e14b743
misraelson <misraelson@gmail.com>
2018-09-11 18:58:07 -0700

    removed skip_before_action from sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
   # resources :activities do
   #   resources :streaks
   # end
