74ec4fd9573af8186acc5a355fc0c9023b3dad0d
misraelson <misraelson@gmail.com>
2018-09-11 19:23:56 -0700

    changed protect_from_forgery method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery unless: -> { request.format.json? }
   include DeviseWhitelist
   acts_as_token_authentication_handler_for User, fallback: :none
 end
